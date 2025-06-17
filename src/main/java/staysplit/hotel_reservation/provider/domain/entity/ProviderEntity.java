2bb8876a897426cfb52deb15858e7cdcb6c4d554
miji <mjkim2304@gmail.com>
2025-06-18 01:47:16 +0900

    feat: Provider 회원가입

---
+++ src/main/java/staysplit/hotel_reservation/provider/domain/entity/ProviderEntity.java
@@ -0,0 +1,25 @@
+package staysplit.hotel_reservation.provider.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import staysplit.hotel_reservation.user.domain.dto.entity.UserEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ProviderEntity {
+
+    @Id
+    private Long id;
+
+    @MapsId // 이렇게 하면 user_id가 이 클래스의 PK
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private UserEntity user;
+
+//    @ManyToOne
+//    @JoinColumn(name = "hotel_id", nullable = false)
+//    private HotelEntity hotel;
+}
