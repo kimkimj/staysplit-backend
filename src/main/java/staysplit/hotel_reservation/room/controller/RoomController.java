e88db007d922bf178487a598c23026a3f09bec93
miji <mjkim2304@gmail.com>
2025-06-24 19:27:42 +0900

    chore: RoomDetailResponse로 DTO이름 변경

--- src/main/java/staysplit/hotel_reservation/room/controller/RoomController.java
+++ src/main/java/staysplit/hotel_reservation/room/controller/RoomController.java
@@ -2,17 +2,13 @@ package staysplit.hotel_reservation.room.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import staysplit.hotel_reservation.common.entity.Response;
 import staysplit.hotel_reservation.room.domain.dto.request.CreateRoomRequest;
 import staysplit.hotel_reservation.room.domain.dto.request.UpdateRoomRequest;
 import staysplit.hotel_reservation.room.domain.dto.response.RoomDeleteResponse;
-import staysplit.hotel_reservation.room.domain.dto.response.RoomInfoResponse;
+import staysplit.hotel_reservation.room.domain.dto.response.RoomDetailResponse;
 import staysplit.hotel_reservation.room.service.RoomService;
 
 @RestController
@@ -23,15 +19,15 @@ public class RoomController {
     private final RoomService roomService;
 
     @PostMapping
-    public Response<RoomInfoResponse> createRoom(@Valid @RequestBody CreateRoomRequest request, Authentication authentication) {
-        RoomInfoResponse infoResponse = roomService.createRoom(authentication.getName(), request);
+    public Response<RoomDetailResponse> createRoom(@Valid @RequestBody CreateRoomRequest request, Authentication authentication) {
+        RoomDetailResponse infoResponse = roomService.createRoom(authentication.getName(), request);
         return Response.success(infoResponse);
     }
 
     @GetMapping("/{roomId}")
-    public Response<RoomInfoResponse> getRoomById(@PathVariable Long roomId) {
-        RoomInfoResponse roomInfoResponse = roomService.findRoomById(roomId);
-        return Response.success(roomInfoResponse);
+    public Response<RoomDetailResponse> getRoomById(@PathVariable Long roomId) {
+        RoomDetailResponse RoomDetailResponse = roomService.findRoomById(roomId);
+        return Response.success(RoomDetailResponse);
     }
 
     @DeleteMapping("/{roomId}")
@@ -41,21 +37,11 @@ public class RoomController {
     }
 
     @PutMapping("/{roomId}")
-    public Response<RoomInfoResponse> updateRoom(@PathVariable Long roomId,
+    public Response<RoomDetailResponse> updateRoom(@PathVariable Long roomId,
                                                  @Valid @RequestBody UpdateRoomRequest updateRoomRequest,
                                                  Authentication authentication) {
-        RoomInfoResponse response = roomService.updateRoom(authentication.getName(), updateRoomRequest);
+        RoomDetailResponse response = roomService.updateRoom(authentication.getName(), roomId, updateRoomRequest);
         return Response.success(response);
     }
 
-    //
-    // TODO: HotelController에 옮기고, SecConfig에서 uri 열어두기
-    // GET ALL ROOMS
-    @GetMapping("/hotels/{hotelId}")
-    public Response<Page<RoomInfoResponse>> findAllRoomsByHotelId(@PathVariable Long hotelId,
-                                                                  @PageableDefault(size = 10, sort = "price", direction = Sort.Direction.ASC)
-                                                                  Pageable pageable) {
-        Page<RoomInfoResponse> rooms = roomService.findAllRoomsByHotel(hotelId, pageable);
-        return Response.success(rooms);
-    }
 }
--- src/main/java/staysplit/hotel_reservation/room/domain/dto/response/RoomDetailResponse.java
+++ src/main/java/staysplit/hotel_reservation/room/domain/dto/response/RoomDetailResponse.java
@@ -2,17 +2,21 @@ package staysplit.hotel_reservation.room.domain.dto.response;
 
 import staysplit.hotel_reservation.room.domain.RoomEntity;
 
-public record RoomInfoResponse(
+public record RoomDetailResponse(
         Long hotelId,
         String hotelName,
+        Long roomId,
         String roomType,
+        Integer quantity,
         Integer maxOccupancy,
         Integer price
 ) {
-    public static RoomInfoResponse from(RoomEntity room) {
-        return new RoomInfoResponse(room.getHotel().getHotelId(),
+    public static RoomDetailResponse from(RoomEntity room) {
+        return new RoomDetailResponse(room.getHotel().getHotelId(),
                 room.getHotel().getName(),
+                room.getId(),
                 room.getRoomType(),
+                room.getQuantity(),
                 room.getMaxOccupancy(),
                 room.getPrice());
     }
