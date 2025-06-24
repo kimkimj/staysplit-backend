938e6823d9d6d91201a1a35f4012399ece8cbeae
miji <mjkim2304@gmail.com>
2025-06-24 19:43:48 +0900

    chore: CustomerRepository에 이메일로 Customer 찾는 기능 추가

--- src/main/java/staysplit/hotel_reservation/customer/repository/CustomerRepository.java
+++ src/main/java/staysplit/hotel_reservation/customer/repository/CustomerRepository.java
@@ -1,6 +1,8 @@
 package staysplit.hotel_reservation.customer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import staysplit.hotel_reservation.customer.domain.entity.CustomerEntity;
 import staysplit.hotel_reservation.user.domain.entity.UserEntity;
@@ -12,5 +14,9 @@ public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> 
     Optional<CustomerEntity> findByUser(UserEntity user);
     boolean existsByNickname(String nickname);
 
+    @Query("SELECT c FROM CustomerEntity c WHERE c.user.email = :email")
+    Optional<CustomerEntity> findByUserByEmail(@Param("email") String email);
+
 
 }
+d
