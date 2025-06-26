72e76935d1e8daf625b7c52ef91fe44cec7afc24
Jay-Park0920 <pjy9020@gmail.com>
2025-06-26 19:14:15 +0900

    fix: 리뷰api 수정

--- src/main/java/staysplit/hotel_reservation/review/repository/ReviewRepository.java
+++ src/main/java/staysplit/hotel_reservation/review/repository/ReviewRepository.java
@@ -3,13 +3,11 @@ package staysplit.hotel_reservation.review.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import staysplit.hotel_reservation.review.domain.entity.ReviewEntity;
 
-import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
