0c5f182d2710773904eecb07da18eb4024ebbd9b
miji <mjkim2304@gmail.com>
2025-06-30 21:29:17 +0900

    fix: 로그인할 때 사용자 역할도 반환하도록 변경

--- src/main/java/staysplit/hotel_reservation/user/controller/UserController.java
+++ src/main/java/staysplit/hotel_reservation/user/controller/UserController.java
@@ -5,9 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import staysplit.hotel_reservation.common.entity.Response;
-import staysplit.hotel_reservation.provider.service.ProviderService;
 import staysplit.hotel_reservation.user.domain.dto.request.LoginRequest;
 import staysplit.hotel_reservation.user.domain.dto.request.PasswordUpdateRequest;
+import staysplit.hotel_reservation.user.domain.dto.response.UserLoginResponse;
 import staysplit.hotel_reservation.user.service.UserService;
 
 @RestController
@@ -15,16 +15,13 @@ import staysplit.hotel_reservation.user.service.UserService;
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
-    private final ProviderService providerService;
-
 
     // 일반 로그인, 소셜 로그인 아님
     @PostMapping("/login")
-    public Response<String> login(@Valid @RequestBody LoginRequest loginRequest) {
-        String jwt = userService.login(loginRequest);
-        return Response.success(jwt);
+    public Response<UserLoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        UserLoginResponse response = userService.login(loginRequest);
+        return Response.success(response);
     }
-
 
     // 비밀 번호 변경
     @PutMapping("/pw")
