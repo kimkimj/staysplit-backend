c7e77b8bc63b3c5a84bf0afaca2768cee571cb56
miji <mjkim2304@gmail.com>
2025-07-06 23:47:34 +0900

    refactor: 패키지 구조 및 호텔의 모든 방 조회 api 위치 변경

--- src/main/java/staysplit/hotel_reservation/hotel/controller/HotelController.java
+++ src/main/java/staysplit/hotel_reservation/hotel/controller/HotelController.java
@@ -3,6 +3,7 @@ package staysplit.hotel_reservation.hotel.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,8 @@ import staysplit.hotel_reservation.common.entity.Response;
 import staysplit.hotel_reservation.hotel.dto.request.*;
 import staysplit.hotel_reservation.hotel.dto.response.*;
 import staysplit.hotel_reservation.hotel.service.HotelService;
+import staysplit.hotel_reservation.room.dto.response.RoomInfoResponse;
+import staysplit.hotel_reservation.room.service.RoomService;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ import staysplit.hotel_reservation.hotel.service.HotelService;
 public class HotelController {
 
     private final HotelService hotelService;
+    private final RoomService roomService;
 
     //호텔 생성
     @PostMapping("/")
@@ -56,5 +60,14 @@ public class HotelController {
 
         DeleteHotelResponse response = hotelService.deleteHotel(hotelId, authentication.getName());
         return Response.success(response);
+    }
+
+    // 호텔의 모든 방 조회
+    @GetMapping("/{hotelId}/rooms")
+    public Response<Page<RoomInfoResponse>> findAllRoomsByHotelId(@PathVariable Integer hotelId,
+                                                                  @PageableDefault(size = 10, sort = "price", direction = Sort.Direction.ASC)
+                                                                  Pageable pageable) {
+        Page<RoomInfoResponse> rooms = roomService.findAllRoomsByHotel(hotelId, pageable);
+        return Response.success(rooms);
     }
 }
